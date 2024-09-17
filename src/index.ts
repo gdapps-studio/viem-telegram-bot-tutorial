0b2a0ce1705f932cad8be658dbeeb456c4ac9a32
gio-shara-code <giorgi.sharashenidze@code.berlin>
2024-09-18 01:36:05 +0200

    comments

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,23 @@ import {DESCRIPTION, ENDING_TEXT, TITLE} from "./constants.js";
 
 const bot = new Bot(env.BOT_TOKEN);
 
+// TODO maybe automate the process of adding a command and extended properties and based on the config code takes care of displaying the buttons, creating callback queries and implementing actions
+// TODO maybe wrap a command in a class where all the logic leaves.
+
+// TODO add third action here
 const COMMANDS = {
     readBlockNumber: 'readBlockNumber',
     getTransactionCount: 'getTransactionCount',
 }
 
+// TODO finalise viem public calls
 bot.callbackQuery(COMMANDS.readBlockNumber, async (ctx) => {
     await ctx.answerCallbackQuery("reading block number")
 })
 
 bot.callbackQuery(COMMANDS.getTransactionCount, async (ctx) => {
     await ctx.answerCallbackQuery("get transaction count")
+    // TODO read user address using telegram bot
 })
 
 bot.command('start', async (ctx) => {
