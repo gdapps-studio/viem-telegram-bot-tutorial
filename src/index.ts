1d76191ea5403736cf112513cacf588368477237
gio-shara-code <giorgi.sharashenidze@code.berlin>
2024-09-18 11:07:24 +0200

    base command dynamic setup

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {Bot, InlineKeyboard} from "grammy";
+import {Bot, Context, InlineKeyboard} from "grammy";
 import env from "./env.js";
 import {DESCRIPTION, ENDING_TEXT, TITLE} from "./constants.js";
 
@@ -7,26 +7,50 @@ const bot = new Bot(env.BOT_TOKEN);
 // TODO maybe automate the process of adding a command and extended properties and based on the config code takes care of displaying the buttons, creating callback queries and implementing actions
 // TODO maybe wrap a command in a class where all the logic leaves.
 
+type Actions = 'readBlockNumber' | 'getBalance' | 'getTransactionCount'
+type ActionObject = {
+    key: Actions,
+    name: string,
+    action: (ctx: Context) => (void | Promise<void>)
+}
 // TODO add third action here
-const COMMANDS = {
-    readBlockNumber: 'readBlockNumber',
-    getTransactionCount: 'getTransactionCount',
+const COMMANDS: Record<Actions, ActionObject> = {
+    readBlockNumber: {
+        key: 'readBlockNumber', action:async  (ctx: Context) => {
+            console.log('reading block number')
+            await ctx.reply('Reading block number')
+        },
+        name: 'Read Block Number'
+    },
+    getBalance: {
+        key: 'getBalance', action: (ctx: Context) => {
+            console.log('getting balance')
+
+        },
+        name: 'Get Balance'
+    },
+    getTransactionCount: {
+        key: 'getTransactionCount', action: (ctx: Context) => {
+            console.log('transaction count')
+
+        },
+        name: 'Get Transaction Count'
+    },
 }
 
-// TODO finalise viem public calls
-bot.callbackQuery(COMMANDS.readBlockNumber, async (ctx) => {
-    await ctx.answerCallbackQuery("reading block number")
-})
 
-bot.callbackQuery(COMMANDS.getTransactionCount, async (ctx) => {
-    await ctx.answerCallbackQuery("get transaction count")
-    // TODO read user address using telegram bot
+Object.keys(COMMANDS).forEach((cKey) => {
+    const command = COMMANDS[cKey as Actions]
+    bot.callbackQuery(command.key, async (ctx) => {
+        await ctx.answerCallbackQuery(`You clicked on ${command.name}`)
+        await command.action(ctx)
+    })
 })
 
 bot.command('start', async (ctx) => {
     const keyboard = new InlineKeyboard()
-    Object.values(COMMANDS).forEach((keyboardElement) => {
-        keyboard.row().text(keyboardElement, keyboardElement)
+    Object.values(COMMANDS).forEach((c) => {
+        keyboard.row().text(c.name, c.key)
     })
 
     await ctx.reply(`
